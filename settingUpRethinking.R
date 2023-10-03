954b3dd169bdba5aacf0d787ec4b427e085892ba
Cristian Stark <cristian.stark@bayer.com>
2023-10-03 09:33:33 -0700

    Update settingUpRethinking.R

--- settingUpRethinking.R
+++ settingUpRethinking.R
@@ -1,6 +1,7 @@
 # we recommend running this is a fresh R session or restarting your current session
 install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
-cmdstanr::install_cmdstan()
+cmdstanr::install_cmdstan(dir="C:/Users/valen/")
+#cmdstanr::set_cmdstan_path(path="C:/Users/valen/")
 
 install.packages("coda")
 install.packages("devtools")
