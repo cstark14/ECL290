d9573021f7f35cb3c525c179ef58ba56a0b155d1
Cristian Stark <cristian.stark@bayer.com>
2023-10-04 20:04:21 -0700

    adding work from class

--- Lecture2_3_globeTossing_postDist.R
+++ Lecture2_3_globeTossing_postDist.R
@@ -25,7 +25,8 @@ compute_posterior_sampleList <- function(the_sample,poss=c(0,0.25,0.5,0.75,1)){
 }
 
 compute_posterior <- function(sumW,sumL,poss=c(0,0.25,0.5,0.75,1)){
-  ways <- sapply( p , function(q) (q*4)^W * ((1-q)*4)^L )
+  ### assuming prior here is a uniform flat line
+  ways <- sapply( poss , function(q) (q*4)^sumW * ((1-q)*4)^sumL )
   post <- ways/sum(ways)
   #bars <- sapply(post,function(q) make_bar(q))
   data.frame(poss,ways,post=round(post,3))
@@ -63,8 +64,45 @@ sim_globe2 <- function( p=0.7 , N=9 , x=0.1 ) {
 ##### assignment questions ####
 ### My answer for assignment 1
 compute_posterior(sumW=4,sumL=11)
+
+####
+#posterior is (likelihood fx x prior fx)/normalization which gives us post
+### Dan Runcie answer:
+##### likelihood (p,w,l): given p, how likely is it w and l. 
+# dnorm(4,15,p) ### probability of 4 out of 15 given p globe/die
+#likelihood <- function(W,L,p) dnorm(W,L,p)
+#prior <- function(p) return(1)
+#prior <- function(p) ifelse(p<0.5,0,1)
+#candidates <- seq(0,1,length=2000)
+#unstandard_posterior = likelihood(4,15,candidates)*prior(candidates)
+#plot(candidates,unstandard_posterior)
+
 #### The answer given. Doesn't make sense to me because the question didn't ask to change the number of sides
-compute_posterior(sumW=4,sumL=11,poss=seq(from=0,to=1,len=11))
+post <- compute_posterior(sumW=4,sumL=11,poss=seq(from=0,to=1,by=0.1))
+plot(post$poss,post$post)
+
+#### Question2:
+## becomes a binomial distribution if we "know" prob
+dbinom(0,5,prob=0.7)
+dbinom(4,5,prob=0.7)
+plot(dbinom(0:5,5,prob=0.7))
+
+###
+rbinom(1,5,prob=0.6) ### simulates tossing once out of 5 at prob p
+### do num simulations based on how likely parameter/prob is
+
+#### vector of poss where each has a prob, get 10000 from this vector at prob relative freq
+samples_of_p = sample(post$poss,1e4,replace=T,prob=post$post)
+hist(samples_of_p)
+sim_5_toss <- rbinom(length(samples_of_p),5,prob=samples_of_p)
+head(sim_5_toss)
+hist(sim_5_toss,prob=T)
+### point estimate
+hist(rbinom(1e4,5,prob=mean(samples_of_p)))
+
+#### Question 3 
+mean(sim_5_toss >=3)
+
 
 p_sample <- rbeta(1e4,4+1,11+1)
 W_sim <- rbinom(1e4,size=5,p=p_samples)
